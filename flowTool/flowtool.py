49c747ce4f5fa2c1df63739e39dceff971ec139a
chenp <641760512@qq.com>
2023-03-20 10:55:21 +0800

    目录调整

--- flowTool/flowtool.py
+++ flowTool/flowtool.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'flowtool.ui'
+# Form implementation generated from reading ui file 'json2jmx.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
